1b75e0d4da50bd5560c291ce1b9ecb6538ea9517
ndelurgio <delurgion@gmail.com>
2022-04-07 15:20:52 -0500

    semi-working model. still some issues

--- TurtlebotModel_Python/controllers.py
+++ TurtlebotModel_Python/controllers.py
@@ -2,44 +2,46 @@ import numpy as np
 import cvxpy as cvx
 
 def cvx_controller(x_chaser_k, x_target, N, dt):
-    B_bar = np.array([[1, 0],
-                      [0, np.cos(x_chaser_k[0])],
-                      [0, np.sin(x_chaser_k[0])]])
-    Q = np.diag([0, 1, 1])
+    # N is the number of controller commands, there are N+1 states from integration
+    B_bar = np.array([[1.0, 0.0],
+                      [0.0, np.cos(x_chaser_k[0])],
+                      [0.0, np.sin(x_chaser_k[0])]])
+    Q = np.diag([0.0, 1.0, 1.0])
     R = 2
     
     uw_max = np.pi/2
     us_max = .15
     
     ref_angle_I = np.arctan2(x_target[2] - x_chaser_k[2]*np.ones((1,len(x_target[0]))),x_target[1] - x_chaser_k[1]*np.ones((1,len(x_target[0]))))
-    u_mat = cvx.Variable((N-1,2))
-    x_chaser = cvx.Variable((N,3))
+    u_mat = cvx.Variable((2,N))
+    x_chaser = cvx.Variable((3,N+1))
     J = 0
     for n in range(N):
         # import pdb;pdb.set_trace()
         # J += (x_target[:,n] - x_chaser[n])@Q@(x_target[:,n] - x_chaser[n])
-        J += Q[1,1]*(x_target[1,n]-x_chaser[n,1])**2 + Q[2,2]*(x_target[2,n]-x_chaser[n,2])**2
+        J += cvx.sum_squares(Q@(x_target[:,n]-x_chaser[:,n]))
         
-        J += R*(ref_angle_I[0,n] - x_chaser[n,0])**2
+        J += R*(ref_angle_I[0,n] - x_chaser[0,n])**2
         
     obj = cvx.Minimize(J)
     
-    constr = []
-    x_chaser[0,:] == x_chaser_k
+    constr = [x_chaser[:,0] == x_chaser_k]
     
-    for t in range(N-1):
+    
+    for t in range(N):
         # import pdb;pdb.set_trace()
-        constr += [x_chaser[t+1] == x_chaser[t] + dt*B_bar@u_mat[t],
-                  u_mat[t,0]<=uw_max, u_mat[t,0]>=-uw_max,
-                  u_mat[t,1]<=us_max, u_mat[t,1]>=0]#,
-                #   (u_mat[t,0]/us_max)**2 + (u_mat[t,1]/uw_max)**2 <= .707]
+        constr += [x_chaser[:,t+1] == x_chaser[:,t] + dt*B_bar@u_mat[:,t],
+                  u_mat[0,t]<=uw_max, u_mat[0,t]>=-uw_max,
+                  u_mat[1,t]<=us_max, u_mat[1,t]>=0,
+                  (u_mat[0,t]/us_max)**2 + (u_mat[1,t]/uw_max)**2 <= np.sqrt(2)]
     
     prob = cvx.Problem(obj, constr)
-    print(prob)
     result = prob.solve()
-    return u_mat.value
+    # print(J.value)
+    # import pdb;pdb.set_trace()
+    return u_mat.value.T
 
-xt = np.array([[0,0,0,0],[0,0,0,0],[0.0,0.05,0.1,0.15]])
-a = cvx_controller(np.array([0,0,0]), xt, 4, 1)
-print(a)
+# xt = np.array([[0.0,0.0,0.0,0.0],[0.1,0.15,0.2,0.25],[0.0,0.0,0.0,0.0]])
+# a = cvx_controller(np.array([0.0,0.0,0.0]), xt, 4, 1.0)
+# print(a)
     #https://colab.research.google.com/github/cvxgrp/cvx_short_course/blob/master/intro/control.ipynb
--- TurtlebotModel_Python/turtlebotsim.py
+++ TurtlebotModel_Python/turtlebotsim.py
@@ -7,16 +7,16 @@ from controllers import cvx_controller
 import matplotlib.pyplot as plt
 
 #Chaser initial state
-x0c = np.array([np.pi,0,0])
+x0c = np.array([np.pi/2,0.0,0.0])
 u0c = np.array([0,0])
 
 #Define reference trajectory
 tf = 16
-x0t = np.array([np.pi,0.1,0.1])
-t_ref = np.array([0,5,6,11,12,tf])
-tt = np.array([x0t[0], x0t[0], x0t[0]-np.pi,x0t[0]-np.pi,x0t[0],x0t[0]])
-xt = np.array([x0t[1], x0t[1], x0t[1],x0t[1]+.25,x0t[1]+.25,x0t[1]+.25])
-yt = np.array([x0t[2], x0t[2]+.25, x0t[2]+.25,x0t[2]+.25,x0t[2]+.25,x0t[2]+.4])
+x0t = np.array([np.pi/2,0.1,0.1])
+t_ref = np.array([0,4,5,9,10,tf])
+tt = np.array([x0t[0], x0t[0], x0t[0]-np.pi/2,x0t[0]-np.pi/2,x0t[0],x0t[0]])
+xt = np.array([x0t[1], x0t[1], x0t[1],x0t[1]+.2,x0t[1]+.2,x0t[1]+.2])
+yt = np.array([x0t[2], x0t[2]+.2, x0t[2]+.2,x0t[2]+.2,x0t[2]+.2,x0t[2]+.5])
 rtraj = np.array([tt,xt,yt]).T
 
 target_state = interp1d(t_ref, rtraj.T)
@@ -27,13 +27,13 @@ dt_sim = 0.01
 #Time duration for ZOH controller commands
 dt_ctrl = .2
 #Time duration between reoptimizing
-dt_opt = 1
+dt_opt = 1.0
 
 ovsf = int(dt_ctrl/dt_sim)
 
 N = 10
 if(N*dt_ctrl < dt_opt):
-    print("Error: control horiziation insufficient for optimizer")
+    print("Error: control horizon insufficient for optimizer")
     quit()
 
 #Generate timespan arrays
@@ -43,32 +43,63 @@ tspan_opt = np.arange(0,tf,dt_opt)
 tspan_ode = np.arange(0,dt_ctrl, dt_sim)
 
 xk = x0c
-x_hist = np.array([x0c])
-u_hist = np.array([u0c])
-for k in range(len(tspan_opt)-1):
+x_hist = np.empty((0,3))
+u_hist = np.empty((0,2))
+for k in range(len(tspan_opt)):
+    
     if(tspan_opt[k] + N*dt_ctrl < tf):
         x_target = target_state(np.arange(tspan_opt[k],tspan_opt[k] + N*dt_ctrl,dt_ctrl))
+        # import pdb; pdb.set_trace()
     else:
         x_target = (target_state(tspan_opt[-1])*np.ones((N,3))).T
     
     uk = cvx_controller(xk, x_target,N,dt_ctrl)
-    
     xc = xk
-    for c in range(len(tspan_ctrl)-1):
+    for c in range(len(tspan_ctrl)):
         #ODE45 call here
-        x = odeint(dynamics,xc,tspan_ode,args = (uk[c],))
-        x_hist = np.concatenate((x_hist,x[0:-1]))
+        x_ode = odeint(dynamics,xc,tspan_ode,args = (uk[c],))
+        # import pdb;pdb.set_trace()
+
+        x_hist = np.concatenate((x_hist,x_ode))
         u_hist = np.concatenate((u_hist,uk[c]*np.ones((ovsf,2))))
-        xc = x[-1]
+        xc = x_ode[-1]
         #Append xhist and u hist
         #xc = x(end,:)
     xk = xc
 #append final state vector to x_hist
-np.concatenate((x_hist,np.array([xk])))
-#import pdb; pdb.set_trace()
+# np.concatenate((x_hist,np.array([xk])))
+# import pdb; pdb.set_trace()
 #plot stuff
 plt.figure(figsize=[8,5])
 plt.scatter(x_hist.T[:][1],x_hist.T[:][2])
 plt.scatter(target_state(tspan_sim)[:][1],target_state(tspan_sim)[:][2])
 plt.axis('equal')
+
+# import pdb; pdb.set_trace()
+plt.figure(figsize=[8,5])
+plt.subplot(3,1,1)
+plt.title("Target vs. Chaser State")
+plt.plot(tspan_sim,x_hist.T[:][0])
+plt.plot(t_ref,rtraj[:,0])
+plt.ylabel("Theta (rad)")
+plt.subplot(3,1,2)
+plt.plot(tspan_sim,x_hist.T[:][1])
+plt.plot(t_ref,rtraj[:,1])
+plt.ylabel("X Position (m)")
+plt.subplot(3,1,3)
+plt.plot(tspan_sim,x_hist.T[:][2])
+plt.plot(t_ref,rtraj[:,2])
+plt.ylabel("Y Position (m)")
+plt.xlabel("Time (s)")
+
+plt.figure(figsize=[8,5])
+plt.subplot(2,1,1)
+plt.title("Control Commands")
+plt.plot(tspan_sim,u_hist[:,0])
+plt.ylabel("u_w")
+plt.subplot(2,1,2)
+plt.plot(tspan_sim,u_hist[:,1])
+plt.ylabel("u_s")
+plt.xlabel("Time (s)")
+
 plt.show()
